eb43a5c8b1f8393bfb741c58fcbabd973241eaa9
brentlaster <bcl@nclasters.org>
2021-01-13 18:28:10 -0500

    add test values

--- docker-entrypoint-initdb.d/04-fill-table.sql
+++ docker-entrypoint-initdb.d/04-fill-table.sql
@@ -4,11 +4,9 @@
 
 LOCK TABLES `agents` WRITE;
 
-INSERT INTO `agents` VALUES (1,'Road Runner','bird','1955-01-20','1995-02-15','Wile E. Coyote','ACME product du jour');
-INSERT INTO `agents` VALUES (2,'Scooby','dog','1969-05-19','2000-02-11','fake ghosts','mask');
-INSERT INTO `agents` VALUES (3,'Perry','platypus','2013-01-20','2015-04-09','H. Doofensmirtz','...inator');
-INSERT INTO `agents` VALUES (4,'Mr. Krabs','crab','2010-06-17','2014-07-07','Plankton','various');
-INSERT INTO `agents` VALUES (5,'Bugs Bunny','rabbit','1966-05-22','1988-04-15','E. Fudd','wabbit gun');
+INSERT INTO `agents` VALUES (1,'(Test 2) Mr. Krabs','crab','2010-06-17','2014-07-07','Plankton','various');
+INSERT INTO `agents` VALUES (2,'(Test 2) Bugs Bunny','rabbit','1966-05-22','1988-04-15','E. Fudd','wabbit gun');
+INSERT INTO `agents` VALUES (3,'(Test 2) Woody Woodpecker','bird','1959-05-22','1979-04-15','Buzz Buzzard','menacing stare');
 
 UNLOCK TABLES;
 
